ded67d868fa1f253d3b58244b64b3cfe7302183e
Ariel <arielgomezmendez94@gmail.com>
2022-07-06 10:55:13 -0400

    Commit

--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
 let urlBaseRaM = "https://rickandmortyapi.com/api/character/";
-let urlBasePokemons = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154";
 
 const getInfo = async() => {
-    //Obtaining  information  from Rick and Morty API
     try{
-        const responseRaM = await fetch(urlBaseRaM);
-        const dataRaM = await responseRaM.json();    
-        //Obtaining a matrix of arrays(2x2) of strings,the entries method helps to convert objects in arrays
-        const generalArrayRaM = Object.entries(dataRaM.results);
-        const arrayRaM = [];
+        //Get gata from API 
+        const responseRickMorty = await fetch(urlBaseRaM);
+        const dataRaM = await responseRickMorty.json();
+        const generalArrayRickMorty = Object.entries(dataRaM.results);
+        console.log(generalArrayRickMorty); 
+        
+        const arrayRickMorty = [];
+        const arrayRickMortyNames = [];  // names array
+        const arrayRickMortyTypes = [];  // types array
+        const arrayRickMortySpecies = []; // speceies array
 
-        for(let i = 0; i < generalArrayRaM.length; i ++){
-            //Obtaining a matrix of arrays(2x2) with values and properties
-            arrayRaM.push(Object.entries(generalArrayRaM[i][1])); 
+        const mainContainer = document.getElementById("Main");
+
+        for(let i = 0; i < generalArrayRickMorty.length; i ++){
+            arrayRickMorty.push(Object.entries(generalArrayRickMorty[i][1]));
+            //Filling arrays
+            arrayRickMortyNames.push(arrayRickMorty[i][1][1]);
+            arrayRickMortyTypes.push(arrayRickMorty[i][4][1]);
+            arrayRickMortySpecies.push(arrayRickMorty[i][3][1]);   
         }
-        console.log(arrayRaM);
-    }
-    
-    catch(error){
-        console.log(error);
-    }
 
-    //Obtaining  information  from  Pokemons API
-    try{
-        const responsePokemons = await fetch(urlBasePokemons);
-        const dataPokemons = await responsePokemons.json();
-        console.log(dataPokemons.results);
-    }
+        const validacion = () => {
+            const foundName = arrayRickMortyNames.find(i => i == input.value);
+            const indexFoundName = arrayRickMortyNames.findIndex(i => i == input.value);
 
+            //Create cards of Rick and Morty
+            const card = document.createElement('article');
+            card.className = "rickMortyCard";
+            const imageRickMortyCharacter = document.createElement("img");
+            imageRickMortyCharacter.className = "characterRickMortyImg";
+            imageRickMortyCharacter.src = `${urlBaseRaM}/avatar/${indexFoundName + 1}.jpeg`;
+            //Add cards
+            mainContainer.append(card);
+            card.append(imageRickMortyCharacter);
+            console.log(foundName);
+            }
+
+            const input = document.getElementById("NameInput");
+            const buttom = document.getElementById('Btn');
+            buttom.addEventListener("click", validacion);
+        }
 
     catch(error){
         console.log(error);
